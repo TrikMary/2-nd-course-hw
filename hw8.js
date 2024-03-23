a003026533a0bb79cce0c2e54780292ad7e18603
TrikMary <marmarshanmarsha@gmail.com>
2024-03-23 19:30:40 +0300

    Выполнен д/з 8

---
+++ hw8.js
@@ -0,0 +1,83 @@
+// "Задание 1"
+console.log("Задание 1")
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+
+ console.log(people.sort((a, b) => a.age - b.age))
+
+
+// "Задание 2"
+console.log("Задание 2")
+
+
+function isPositive(numb) {
+    return numb > 0 ;
+}
+function isMale(person) {
+return person.gender === "male";
+}
+
+function filter(arr, ruleFunction) {
+    const result = [];
+    for (let el of arr) {
+        if (ruleFunction(el)) {
+            result.push(el);
+        }
+    }
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+    
+const peoples = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(peoples, isMale));
+
+// "Задание 3"
+console.log("Задание 3")
+
+let timerId = setInterval(() => console.log(new Date()), 3000);
+setTimeout(() => {clearInterval(timerId); alert ("30 секунд прошло");}, 30000);
+
+// "Задание 4"
+console.log("Задание 4")
+
+function delayForSecond(callback) {
+    setTimeout (callback, 1000);
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+})
+
+// "Задание 5"
+console.log("Задание 5")
+
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Код выше менять нельзя
+
+// изменяем код ниже
+delayForSecond(() => sayHi("Олег"));
--- te.js
+++ te.js
@@ -1,20 +1,78 @@
-const newspaper = {
-	sports: {
-        title : 'ARod Hits Home Run',
-        writers : ['Miramon Nuevo', 'Rick Reilly', 'Woddy Paige']
-    }, 
-	business: {
-        title : 'GE Stock Dips Again',
-	    writers : ['Adam Smith', 'Albert Humphrey', 'Charles Handy']
-    },
-    mov: {
-        title: 'Superman Is A Flop',
-	    writers: ['Rogert Ebert', 'Andrew Sarris', 'Wesley Morris']
-    }
-}
-console.log(newspaper.sports.title); // ARod Hits Home Run
-console.log(newspaper.business.writers[0]); // Adam Smith
-console.log(newspaper['mov']['title']);
+
+
+
+// function log(arrItem) {
+//     console.log("Элемент массива:", arrItem);
+// }
+
+// function each(arr, cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     cb(arr[i]);
+//   }
+// }
+
+// each(["Глеб", "Олег", "Татьяна", "Платон"], log);
+// //
+
+// const fibonacci = [1, 1, 2, 3, 5, 8];
+// const multipleByTwoFib = fibonacci.map((el) => el * 2);
+// console.log(multipleByTwoFib);
+// const plusTenFib = fibonacci.map(el => el + 10);
+// console.log(plusTenFib);
+// const onlyBigFib = fibonacci.filter(el => el > 3);
+// console.log(onlyBigFib);
+
+// const makePizza = function (title, cb) {
+//     console.log(`Заказ на приготовление ${title} получен. Начинаем готовить...`);
+//     setTimeout(cb, 3000);
+// }
+
+// const readBook = function () {
+//     console.log(`Читаю книгу "Колдун и кристалл"`);
+// }
+
+// const eatPizza = function (drink) {
+//     return function() {
+//         console.log (`Ура! Пицца готова, пора подкрепиться! и запить ${drink}`);
+//     }
+// }
+//  makePizza("Пеперонни", eatPizza(`Coca-Cola`));
+//  readBook();
+
+
+
+// const foo = function() {
+//     return "Hello, world";
+// }
+
+// console.log(foo());
+// console.log(foo);
+
+// const runIt = function(fn) {
+//     return fn();
+// }
+
+// console.log(runIt(foo));
+
+
+
+// const newspaper = {
+// 	sports: {
+//         title : 'ARod Hits Home Run',
+//         writers : ['Miramon Nuevo', 'Rick Reilly', 'Woddy Paige']
+//     }, 
+// 	business: {
+//         title : 'GE Stock Dips Again',
+// 	    writers : ['Adam Smith', 'Albert Humphrey', 'Charles Handy']
+//     },
+//     mov: {
+//         title: 'Superman Is A Flop',
+// 	    writers: ['Rogert Ebert', 'Andrew Sarris', 'Wesley Morris']
+//     }
+// }
+// console.log(newspaper.sports.title); // ARod Hits Home Run
+// console.log(newspaper.business.writers[0]); // Adam Smith
+// console.log(newspaper['mov']['title']);
 
 // let arrSum = [3,4,5,6];
 // const sum = ([2],[3],...rest) => {
